abd7a51795b04d484ed27a89855f31d95db0c4b3
Ferry Kobus <ferry@webmonkeys.nl>
2014-11-26 16:51:40 +0100

    Added language packs and automated intialisation

--- panel/apps/authentication/authentication.js
+++ panel/apps/authentication/authentication.js
@@ -70,36 +70,38 @@ Authentication.prototype.go = function(callback) {
 	}
 };
 
+/**
+ *
+ */
 Authentication.prototype.showLoginForm = function(callback) {
-	var fn = jade.compileFile(__dirname + '/jade/login.jade', jade_options);
-	//languages.addTranslation('showLoginForm', {field:{username:{label:'Username'},password:{label:'Password'}},button:{login:{label:'Sign in'}},links:{signup:{label:'Signup'},forgot_password:{label:'Forgot password?'}}}, 'en');
-	//languages.addTranslation('showLoginForm', {field:{username:{label:'Gebruikersnaam'},password:{label:'Wachtwoord'}},button:{login:{label:'Inloggen'}},links:{signup:{label:'Inschrijven'},forgot_password:{label:'Wachtwoord vergeten?'}}}, 'nl');
 
 	languages.getTranslation('showLoginForm', function(data) {
-		var body = fn(data);
+		var fn = jade.compileFile(__dirname + '/jade/login.jade', jade_options),
+			body = fn(data);
 		callback(body);
 	});
 }
 
-
+/**
+ *
+ */
 Authentication.prototype.showRecoveryForm = function(callback) {
-	var fn = jade.compileFile(__dirname + '/jade/recovery.jade', jade_options);
-	//languages.addTranslation('showRecoveryForm', {field:{email:{label:'Email address'}},button:{recovery:{label:'Recover'}},links:{signup:{label:'Signup'},login:{label:'Sign in'}}}, 'en');
-	//languages.addTranslation('showRecoveryForm', {field:{email:{label:'E-mail adres'}},button:{recovery:{label:'Herstellen'}},links:{signup:{label:'Inschrijven'},login:{label:'Inloggen'}}}, 'nl');
-
+	
 	languages.getTranslation('showRecoveryForm', function(data) {
-		var body = fn(data);
+		var fn = jade.compileFile(__dirname + '/jade/recovery.jade', jade_options),
+			body = fn(data);
 		callback(body);
 	});
 }
 
+/**
+ *
+ */
 Authentication.prototype.showSignupForm = function(callback) {
-	var fn = jade.compileFile(__dirname + '/jade/signup.jade', jade_options);
-	//languages.addTranslation('showSignupForm', {field:{email:{label:'Email address'},username:{label:'Username'},password:{label:'Password'},confirm:{label:'Confirm password'}},button:{signup:{label:'Sign up'}},links:{login:{label:'Sign in'},forgot_password:{label:'Forgot password?'}}}, 'en');
-	//languages.addTranslation('showSignupForm', {field:{email:{label:'E-mail adres'},username:{label:'Gebruikersnaam'},password:{label:'Wachtwoord'},confirm:{label:'Bevestig wachtwoord'}},button:{signup:{label:'Aanmelden'}},links:{login:{label:'Inloggen'},forgot_password:{label:'Wachtwoord vergeten?'}}}, 'nl');
 	
 	languages.getTranslation('showSignupForm', function(data) {
-		var body = fn(data);
+		var fn = jade.compileFile(__dirname + '/jade/signup.jade', jade_options),
+			body = fn(data);
 		callback(body);
 	});
 }
