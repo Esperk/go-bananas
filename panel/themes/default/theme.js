fa6db36f0aa014e3c3fa46eebe2c3b6c1c681b83
Ferry Kobus <ferry@webmonkeys.nl>
2014-11-27 01:04:59 +0100

    Start with dashboard

---
+++ panel/themes/default/theme.js
@@ -0,0 +1,17 @@
+/*
+ * Go Bananas
+ *
+ * Copyright (c) 2014 Ferry Kobus - Webmonkeys
+ */
+
+"use strict";
+
+
+function Theme() {
+	return function(req, res, callback) {
+		callback();
+	}
+}
+
+
+module.exports = exports = Theme;
