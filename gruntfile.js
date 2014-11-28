6c19edd23290be2fa5c3be0b75c77b3ab821d539
Ferry Kobus <ferry@webmonkeys.nl>
2014-11-28 01:39:05 +0100

    Added bower and bower install to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -7,15 +7,22 @@
 "use strict";
 
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-compass');
+	require('load-grunt-tasks')(grunt);
 	grunt.initConfig({
 		uglify: {
-			my_target: {
+			bananas: {
 				files: {
 					'panel/themes/default/js/script.js': ['panel/themes/default/js/dev/*.js'],
 					'public/themes/*/js/script.js': ['public/themes/*/js/dev/*.js']
+				}
+			},
+			bower: {
+				options: {
+					mangle: true,
+					compress: true
+				},
+				files: {
+					'public/assets/bower.min.js': 'public/assets/_bower.js'
 				}
 			}
 		},
@@ -29,14 +36,91 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			scripts: {
-				files: ['panel/themes/default/js/dev/*.js', 'public/themes/*/js/dev/*.js'], 
-				tasks: ['uglify']
+				files: ['panel/themes/*/js/dev/*.js', 'public/themes/*/js/dev/*.js'], 
+				tasks: ['uglify:bananas']
 			},
 			sass: {
 				files: ['panel/themes/default/sass/*.scss'],
 				tasks: ['compass:dev']
 			}
 		}
-	})
-	grunt.registerTask('default', 'watch');
+	});
+	// default task, just run watch for compass and js uglify
+	grunt.registerTask('default', ['watch']);
+
+	// install bower deps automatic
+	grunt.registerTask('install', 'Install bower dependencies', function() {
+		var bower = require('bower'),
+			fs = require('fs'),
+			glob = require('glob'),
+			wrench = require('wrench'),
+			exec = require('child_process').exec,
+			done = this.async();
+
+		bower
+			.commands
+			.install([], {})
+			.on('log', function(result) {
+				grunt.log.writeln(['bower', result.id.cyan, result.message].join(' '));
+			})
+			.on('error', function() {
+				done(false);
+			})
+			.on('end', function(results) {
+				for (var key in results) {
+					var pack = results[key],
+						main = pack.pkgMeta.main;
+					
+					if (typeof main === 'object') {
+						for (var index in main) {
+							var file = main[index];
+							if (file.match(/([*])$/gm)) {
+								while (file.charAt(0) === '.' || file.charAt(0) === '/') {
+									file = file.substr(1);
+								}
+								var files = glob.sync(pack.canonicalDir + '/' + file);
+								if (typeof files === 'object') {
+									for (var k in files) {
+										var file = files[k].replace(pack.canonicalDir+'/', '');
+										copyMainFile(pack.canonicalDir, file);
+									}
+								}
+							} else {
+								copyMainFile(pack.canonicalDir, file);
+							}
+						}
+					} else {
+						copyMainFile(pack.canonicalDir, main);
+					}
+				}
+				done();
+			});
+
+		// copy files
+		function copyMainFile(sourceDirectory, mainFile) {
+			var parts = mainFile.split('/'),
+				file = parts[parts.length-1],
+				subdir = '',
+				destination = __dirname + '/public/assets/';
+			while (mainFile.charAt(0) === '.' || mainFile.charAt(0) === '/') {
+				mainFile = mainFile.substr(1);
+			}
+			if(~file.indexOf('.js')) {
+				subdir = 'js/';
+			} else if(~file.indexOf('.css')) {
+				subdir = 'css/';
+			} else {
+				parts.pop();
+				subdir = parts.join('/')+'/';
+			}
+			destination += subdir;
+			wrench.mkdirSyncRecursive(destination, '0755');
+			destination += file;
+			if (fs.existsSync(destination)) {
+				fs.unlinkSync(destination);
+			}
+			fs.createReadStream(sourceDirectory + '/' + mainFile).pipe(fs.createWriteStream(destination));
+			grunt.log.writeln(['copied', file.cyan, '/public/assets/' + subdir + file].join(' '));
+		}
+	});
 };
